093cb4a15aecdd6b4112656d24f3006161ae21ae
Domenick Braccia <domenick.braccia@gmail.com>
2023-04-13 01:17:54 -0400

    major updates to parse_time_log.py

--- code/parse_time_log.py
+++ code/parse_time_log.py
@@ -1,6 +1,5 @@
-# import something
 import sys
-import numpy
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -10,32 +9,33 @@ DESCRIPTION
 '''
 
 ## take input from standard input
-week = sys.argv[1] ## ex: 
+time_file = sys.argv[1] ## ex:
 
 ## load dataset(s)
-df_today = pd.read_csv(('time_logs/' + week + '.csv'), sep = ',')
+df = pd.read_csv(time_file, sep = ',')
 
 ## process data
 
 ### creating a separate column with just date created
-date_time = df_today['Created At'].str.split(' ', expand = True)
+date_time = df['Created At'].str.split(' ', expand = True)
 date_time.columns = ['date', 'time']
-df_today['date'] = date_time['date']
-df_today['time'] = date_time['time']
+df['date'] = date_time['date']
+df['time'] = date_time['time']
 
 ### converting Elapsed Time column to datetime format
-df_today['elapsed_time'] = pd.to_datetime(df_today['Elapsed Time'])
-df_today['hours_elapsed'] = (df_today.elapsed_time.dt.hour + df_today.elapsed_time.dt.minute/60 + df_today.elapsed_time.dt.second / 3600)
+df['elapsed_time'] = pd.to_datetime(df['Elapsed Time'])
+df['hours_elapsed'] = (df.elapsed_time.dt.hour + df.elapsed_time.dt.minute/60 + df.elapsed_time.dt.second / 3600)
 
 ### grouping data by relevant variables
-mat_today = df_today.groupby(['date', 'Text'])['hours_elapsed'].sum().unstack().fillna(0)
+mat = df.groupby(['date', 'Text'])['hours_elapsed'].sum().unstack().fillna(0)
+# print(mat)
 
 ## plot data
 # pal = sns.color_palette("Set2", as_cmap = False)
-pal = sns.color_palette("pastel", mat_today.shape[1])
+pal = sns.color_palette("pastel", mat.shape[1])
 
-mat_today.plot(kind = 'bar', stacked = True, color = pal)
-# mat_today.plot(kind = 'bar', stacked = True)
+mat.plot(kind = 'bar', stacked = True, color = pal)
+# mat.plot(kind = 'bar', stacked = True)
 plt.legend(loc = 'best', bbox_to_anchor = (1.0, 1.00))
 plt.title('Hours spent on different projects')
 plt.ylabel('hours spent')
@@ -43,4 +43,8 @@ plt.xticks(rotation = 45, ha = 'center')
 plt.tight_layout()
 
 ## output data to excel or plotting software
-plt.savefig('plots/' + week + '.png')
+time_period = time_file.split('/')[1].split('.')[0]
+# print(time_period)
+plot_path = 'plots/' + time_period + '.png'
+plt.savefig(plot_path)
+print('plot saved to: ' + plot_path)
